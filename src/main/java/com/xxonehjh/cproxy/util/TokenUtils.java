2728aab5fef50df95e3749b7b9624d10af10b138
xxonehjh <xxonehjh@163.com>
2017-06-11 16:33:20 +0800

    优化权限验证 精确到分钟经常验证失败 Signed-off-by: xxonehjh <xxonehjh@163.com>

--- src/main/java/com/xxonehjh/cproxy/util/TokenUtils.java
+++ src/main/java/com/xxonehjh/cproxy/util/TokenUtils.java
@@ -3,8 +3,6 @@ package com.xxonehjh.cproxy.util;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import com.xxonehjh.cproxy.Constants;
-
 public class TokenUtils {
 
 	public static String encrypt(String token) {
@@ -12,15 +10,12 @@ public class TokenUtils {
 	}
 
 	public static String encrypt(String token, Date date) {
-		SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmm");
+		SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHH");
 		return MD5.md5(token + df.format(date));
 	}
 
 	public static boolean validate(String tokenEncryption, String token) {
 		if (encrypt(token, new Date()).equals(tokenEncryption)) { // 当前时间
-			return true;
-		}
-		if (encrypt(token, new Date(System.currentTimeMillis() - Constants.ONE_MINUTE)).equals(tokenEncryption)) {// 一分钟之前
 			return true;
 		}
 		return false;
