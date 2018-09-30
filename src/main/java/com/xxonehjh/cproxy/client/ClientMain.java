eca1ab29facca536fcbdd5f0d0ae5c817987da08
xxonehjh <xxonehjh@163.com>
2018-09-30 11:43:33 +0800

    update

--- src/main/java/com/xxonehjh/cproxy/client/ClientMain.java
+++ src/main/java/com/xxonehjh/cproxy/client/ClientMain.java
@@ -6,9 +6,24 @@ import com.xxonehjh.cproxy.client.core.Client;
 public class ClientMain {
 
 	public static void main(String[] args) throws Exception {
-		String config = args.length>0?args[0]:Constants.CLINET_CONFIG;
-		ClientContext context = new ClientContext(config);
-		new Client().start(context);
+		if (args.length == 0) {
+			ClientContext context = new ClientContext(Constants.CLINET_CONFIG);
+			new Client().start(context);
+		} else {
+			for (String item : args) {
+				final String config = item;
+				new Thread() {
+					public void run() {
+						ClientContext context = new ClientContext(config);
+						try {
+							new Client().start(context);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}.start();
+			}
+		}
 	}
 
 }
